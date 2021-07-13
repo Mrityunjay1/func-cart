2bc663ab1c82322dfacf8f0b4498c2e0738b6af9
Mrityunjay_Singh <singhmrityunjay32@gmail.com>
2021-07-13 16:45:39 +0530

    new-commit

--- src/cart-items.js
+++ src/cart-items.js
@@ -22,5 +22,13 @@ export default [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
     amount: 1
+  },
+  {
+    id: 4,
+    title: "Imac",
+    price: 967.30,
+    img:
+      "https://images.unsplash.com/photo-1527443154391-507e9dc6c5cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    amount: 1
   }
 ];
